3cfcfa230e5d4a09884f6ceb859d41bdc7d3044d
bragimova <i.zhanara.k@gmail.com>
2018-05-01 11:16:07 -0700

    ADDED COMMEMTS AND SHARING via EMAIL

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -26,6 +27,8 @@ class Post(models.Model):
     objects = models.Manager() # The default manager.
     published = PublishedManager() # The Dahl-specific manager.
 
+    tags = TaggableManager()
+
     class Meta:
         ordering = ('-publish',)
 
@@ -37,3 +40,19 @@ class Post(models.Model):
                                                  self.publish.strftime('%m'),
                                                  self.publish.strftime('%d'),
                                                  self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
--- mysite/settings.py
+++ mysite/settings.py
@@ -102,3 +102,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'my_account@gmail.com'
+#EMAIL_HOST_PASSWORD = ''
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
